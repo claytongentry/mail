9f965fa58a6e718f69566f1777d3ed0ab28829d1
Clayton Gentry <cg3ntry@gmail.com>
2021-01-23 09:14:53 -0500

    Handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use async_std::io::prelude::*;
+use async_std::net::{TcpListener, TcpStream};
+use futures::stream::StreamExt;
 use std::str;
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 10];
-    let _len = stream.read(&mut buffer);
+async fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).await.unwrap();
+
     let string_value = str::from_utf8(&mut buffer).unwrap();
     println!("Server got bytes: {}", string_value);
+
+    let response = "ack\n";
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
 
-fn main() -> std::io::Result<()> {
+#[async_std::main]
+async fn main() {
     /*
      * IMAPrev1 servers listen on port 143
      * https://tools.ietf.org/html/rfc2060#section-2.1
      */
-    let listener = TcpListener::bind("127.0.0.1:1143")?;
+    let listener = TcpListener::bind("127.0.0.1:1143").await.unwrap();
 
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?);
-    }
-    Ok(())
+    // accept connections concurrently
+    listener
+        .incoming()
+        .for_each_concurrent(/* limit */ None, |tcpstream| async move {
+            let tcpstream = tcpstream.unwrap();
+            handle_client(tcpstream).await;
+        })
+        .await
 }
