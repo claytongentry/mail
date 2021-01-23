f2fa200986dd378ddbf40e8a9dcb25713cd5a8bb
Clayton Gentry <cg3ntry@gmail.com>
2021-01-23 12:36:06 -0500

    Match client inputs

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,24 @@ use async_std::io::prelude::*;
 use async_std::net::{TcpListener, TcpStream};
 use futures::stream::StreamExt;
 use std::io::ErrorKind;
-use std::str;
+use std::string::String;
 
 async fn handle_client(mut stream: TcpStream) {
     loop {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).await.unwrap();
 
-        let string_value = str::from_utf8(&mut buffer).unwrap();
-        println!("Server got bytes: {}", string_value);
+        let command = String::from_utf8(buffer.to_vec()).unwrap();
+        let command = command.trim_matches(char::from(0));
+
+        match command {
+            "CAPABILITY\n" => {
+                println!("Got capability! {:?}", command);
+            }
+            _other => {
+                println!("Huh? {}", command);
+            }
+        };
 
         let response = "ack\n";
         let result = stream.write(response.as_bytes()).await;
